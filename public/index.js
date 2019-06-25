6b6a51c454c58f603d53a3d5cb8b6805b4770e1d
dbobb220 <david.bobb.220@gmail.com>
2019-06-25 11:38:14 -0400

    search funct

--- public/index.js
+++ public/index.js
@@ -2,19 +2,36 @@
 // make section a variable
 let container = document.querySelector('.product_list');
 
+// Add Products to DOM
 
-for (let i = 0; i < products.length; i++) {
-    let product = products[i];
-    console.log(product);
-    let productContainer = document.createElement('div');
-    productContainer.setAttribute('class', 'product');
-    productContainer.innerHTML = `
-    <h3>${product.name}</h3>
-    `
-    container.appendChild(productContainer);
+const loadProducts = (prod) => {
+    for (let i = 0; i < prod.length; i++) {
+        let product = prod[i];
+        console.log(product);
+        let productContainer = document.createElement('div');
+        productContainer.setAttribute('class', 'product');
+        productContainer.innerHTML = `
+        <h3>${product.name}</h3>
+        `
+        container.appendChild(productContainer);
+    }
 }
 
+// TODO: clear previous list to replace with regex-ed filtered
 let searchProduct = () => {
     let searchInput = document.querySelector('#search').value;
-    products.map()
+    let myRegEx = new RegExp(`${searchInput}`, 'gi');
+    let holderArray = [];
+    for (let i = 0; i < products.length; i++) {
+        let productName = products[i].name;
+        if(productName.match(myRegEx)) {
+            holderArray.push(products[i])
+        }
+    }
+    loadProducts(holderArray);
 }
+
+//!! BOOKMARK: Shopping Cart
+
+// initial product load
+loadProducts(products);
