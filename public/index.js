e5c3990c2d43ae7e8603e98da8dd7ecea94764a3
dbobb220 <david.bobb.220@gmail.com>
2019-06-24 21:50:13 -0400

    product list and beg of search

--- public/index.js
+++ public/index.js
@@ -1,2 +1,20 @@
-//stuff
-//more stuff
+// declaring variable
+// make section a variable
+let container = document.querySelector('.product_list');
+
+
+for (let i = 0; i < products.length; i++) {
+    let product = products[i];
+    console.log(product);
+    let productContainer = document.createElement('div');
+    productContainer.setAttribute('class', 'product');
+    productContainer.innerHTML = `
+    <h3>${product.name}</h3>
+    `
+    container.appendChild(productContainer);
+}
+
+let searchProduct = () => {
+    let searchInput = document.querySelector('#search').value;
+    products.map()
+}
