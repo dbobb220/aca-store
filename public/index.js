07ca4173bf7eb85c5ccf1654cd9e9cf701787c19
dbobb220 <david.bobb.220@gmail.com>
2019-06-27 17:32:42 -0500

    add sections before branch to change load logic

--- public/index.js
+++ public/index.js
@@ -5,33 +5,47 @@ let container = document.querySelector('.product_list');
 // Add Products to DOM
 
 const loadProducts = (prod) => {
+    container.innerHTML = '';
     for (let i = 0; i < prod.length; i++) {
         let product = prod[i];
-        console.log(product);
         let productContainer = document.createElement('div');
         productContainer.setAttribute('class', 'product');
         productContainer.innerHTML = `
         <h3>${product.name}</h3>
+        <p>Rating: ${product.rating}</p>
+        <p>Number of Reviews: ${product.reviews.length}</p>
+        <p>Price: ${product.price}</p>
+        <p><button onClick="viewDetail(${product.id})">View Details</button></p>
         `
         container.appendChild(productContainer);
     }
 }
 
-// TODO: clear previous list to replace with regex-ed filtered
 let searchProduct = () => {
     let searchInput = document.querySelector('#search').value;
     let myRegEx = new RegExp(`${searchInput}`, 'gi');
     let holderArray = [];
     for (let i = 0; i < products.length; i++) {
         let productName = products[i].name;
-        if(productName.match(myRegEx)) {
+        let productDesc = products[i].description;
+        if(productName.match(myRegEx) || productDesc.match(myRegEx)) {
             holderArray.push(products[i])
         }
     }
     loadProducts(holderArray);
 }
 
-//!! BOOKMARK: Shopping Cart
+//!! Shopping Cart
+
+let viewCart = () => {
+    //viewCart code
+}
+
+//!! View Details
+
+let viewDetail = (id) => {
+    //code 
+}
 
 // initial product load
 loadProducts(products);
