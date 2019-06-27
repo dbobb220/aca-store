36da238e0ae4c655d96addded0fc51eecf63f010
dbobb220 <david.bobb.220@gmail.com>
2019-06-27 13:46:13 -0500

    materialize

--- public/index.js
+++ public/index.js
@@ -7,31 +7,37 @@ let container = document.querySelector('.product_list');
 const loadProducts = (prod) => {
     for (let i = 0; i < prod.length; i++) {
         let product = prod[i];
-        console.log(product);
         let productContainer = document.createElement('div');
         productContainer.setAttribute('class', 'product');
         productContainer.innerHTML = `
-        <h3>${product.name}</h3>
+        <h5>${product.name}</h5>
+        <p>${product.description}<p>
         `
         container.appendChild(productContainer);
     }
 }
 
-// TODO: clear previous list to replace with regex-ed filtered
-let searchProduct = () => {
+// Search for product using RegEx
+const searchProduct = () => {
     let searchInput = document.querySelector('#search').value;
     let myRegEx = new RegExp(`${searchInput}`, 'gi');
     let holderArray = [];
     for (let i = 0; i < products.length; i++) {
         let productName = products[i].name;
-        if(productName.match(myRegEx)) {
+        let productDesc = products[i].description;
+        if(productName.match(myRegEx) || productDesc.match(myRegEx)) {
             holderArray.push(products[i])
         }
     }
+    container.innerHTML = '';
     loadProducts(holderArray);
 }
 
-//!! BOOKMARK: Shopping Cart
+// View Cart Items
+const viewProduct = () => {
+    
+}
+
 
 // initial product load
 loadProducts(products);
